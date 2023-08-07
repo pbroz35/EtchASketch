5b700c3212e552b1bb7013394d5380036fc8be34
pbroz35 <1brozek.peter@gmail.com>
2023-08-07 18:08:22 -0500

    added grid and title + some functional buttons

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+This project is a web-based implementation of the classic Etch-a-Sketch game, where users can draw on a virtual canvas using their mouse. The project showcases my CSS, HTML, and JavaScript skills. The game provides a simple and interactive user interface, allowing users to create their artistic masterpieces.
+
 # EtchASketch
---
+++ main.js
@@ -0,0 +1,41 @@
+let color='black';
+
+
+function populateBoard(size){
+    let board=document.querySelector('.board');
+    let squares=board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    
+    board.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+
+let amount=size*size;
+
+for(let i=0; i<=amount; i++)
+{
+    let square=document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor='blue';
+    board.insertAdjacentElement("beforeend", square);
+}
+}
+populateBoard(16);
+
+function changeSize(input){
+    populateBoard(input);
+}
+
+function colorSquare(){
+    if(color==='random'){
+        this.style.backgroundColor=`hs1(${Math.random()*360}, 100%, 50%)`;
+    }
+    else{
+        this.style.backgroundColor=color;
+    }
+}
+
+function changeColor(choice){
+    color=choice;
+}
+
+
