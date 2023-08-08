e7be8be31c92ea77db051ae6b1b43eaadc60812f
pbroz35 <1brozek.peter@gmail.com>
2023-08-08 10:45:16 -0500

    fixed random color button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let color='black';
+let color='random';
 
 
 function populateBoard(size){
@@ -11,23 +11,31 @@ function populateBoard(size){
 
 let amount=size*size;
 
-for(let i=0; i<=amount; i++)
-{
+for(let i=0; i<=amount; i++){
     let square=document.createElement('div');
     square.addEventListener('mouseover', colorSquare);
     square.style.backgroundColor='blue';
     board.insertAdjacentElement("beforeend", square);
+    }
 }
-}
+
 populateBoard(16);
 
 function changeSize(input){
-    populateBoard(input);
+    if(input>=2 && input<=100){
+        populateBoard(input);
+    }else{
+        console.log('too many squares')
+    }
 }
 
 function colorSquare(){
-    if(color==='random'){
-        this.style.backgroundColor=`hs1(${Math.random()*360}, 100%, 50%)`;
+    //Generate random color by selecting random RGB values
+    if(color === "random"){
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        this.style.backgroundColor= `rgb(${r}, ${g}, ${b})`;
     }
     else{
         this.style.backgroundColor=color;
