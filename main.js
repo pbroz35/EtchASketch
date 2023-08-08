bb160f5463e15f97f655ac31aab6d64fdd44561e
pbroz35 <1brozek.peter@gmail.com>
2023-08-08 11:21:42 -0500

    added css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let color='random';
-
+let click=true;
 
 function populateBoard(size){
     let board=document.querySelector('.board');
@@ -14,7 +14,7 @@ let amount=size*size;
 for(let i=0; i<=amount; i++){
     let square=document.createElement('div');
     square.addEventListener('mouseover', colorSquare);
-    square.style.backgroundColor='blue';
+    square.style.backgroundColor='white';
     board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -30,15 +30,19 @@ function changeSize(input){
 }
 
 function colorSquare(){
+    
+    if(click){
+
     //Generate random color by selecting random RGB values
-    if(color === "random"){
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        this.style.backgroundColor= `rgb(${r}, ${g}, ${b})`;
-    }
-    else{
-        this.style.backgroundColor=color;
+        if(color === "random"){
+            var r = Math.floor(Math.random() * 256);
+            var g = Math.floor(Math.random() * 256);
+            var b = Math.floor(Math.random() * 256);
+            this.style.backgroundColor= `rgb(${r}, ${g}, ${b})`;
+        }
+        else{
+            this.style.backgroundColor=color;
+            } 
     }
 }
 
@@ -46,4 +50,26 @@ function changeColor(choice){
     color=choice;
 }
 
+function resetBoard(){
 
+    let board=document.querySelector('.board');
+    let squares=board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    
+    populateBoard(16);
+
+}
+
+document.querySelector('body').addEventListener('click',(e)=>{
+    
+    if(e.target.insertAdjacentElement!="button")
+    click=!click;
+    if(click){
+        document.querySelector('.mode').textContent="Mode: Coloring";
+    }else{
+        document.querySelector('.mode').textContent='Mode: Not Coloring'
+    }
+
+
+
+})
